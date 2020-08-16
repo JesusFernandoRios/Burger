0a5194173b6de3111ba09cb98b22d339f80d13c3
Jrios <Jesusfernandorios93@gmail.com>
2020-08-16 00:17:07 -0700

    troubleshooting heroku deplyment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port:3306,
-    user:'root',
-    password:'Wink26er!',
-    database:'burgers_db'
-})
+let connection;
 
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
