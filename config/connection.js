906bc42bab80e4e6c3bb3d0d03fcbd48537c639d
Jrios <Jesusfernandorios93@gmail.com>
2020-08-15 23:45:33 -0700

    Taking steps for deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,16 @@ const connection = mysql.createConnection({
     database:'burgers_db'
 })
 
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Wink26er!',
+        database:'burgers_db'
+    })
+}
 //////
 
 //////
